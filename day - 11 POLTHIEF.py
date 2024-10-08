b785d1a06d20fc41210f4d4ef8df4ad64e19cdad
ShivangiPathak1 <shivangipathak12a@gmail.com>
2024-10-08 18:46:53 +0530

    Add files via upload

---
+++ day - 11 POLTHIEF.py
@@ -0,0 +1,13 @@
+# cook your dish here
+from math import *
+T=int(input())
+
+for i in range(T):
+    X,Y=input().split()
+    x=int(X)
+    y=int(Y)
+    t=abs(y-x)
+    print(t)
+    
+     
+        
